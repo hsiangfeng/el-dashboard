3b48ca99ed738a1555659bc13f6b2e364729a691
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2022-09-06 11:25:49 +0800

    feat: 404 Page

--- src/router/index.js
+++ src/router/index.js
@@ -53,6 +53,11 @@ const routes = [
       },
     ],
   },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import('../views/NotFound.vue'),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,37 @@
+<template>
+  <ElRow class="h-100vh" justify="center" align="middle">
+    <ElCol :md="12">
+      <ElCard>
+        <ElRow>
+          <ElCol class="d-flex-center" :md="12">
+            <ElImage :src="require('@/assets/logo.png')" alt="404" :fit="'fill'" />
+          </ElCol>
+          <ElCol :md="12">
+            <h1>404</h1>
+            <h2>找不到頁面</h2>
+            <p>您要觀看的頁面可能已被刪除、名稱已更改或暫時不可用。</p>
+            <ElButton type="primary" @click="$router.push('/')">Go to Home</ElButton>
+          </ElCol>
+        </ElRow>
+      </ElCard>
+    </ElCol>
+  </ElRow>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+.h-100vh {
+  height: 100vh;
+}
+
+.d-flex-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
